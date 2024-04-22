ec71a7087a8d010346ddddd3b153f4704dea3b6d
alavazjordan <124105392+alavazjordan@users.noreply.github.com>
2024-04-22 01:07:52 +0000

    Add files via upload
    Latest Version of the PianoPilot Demo as of 4/21/2024 9 pm, I have made the layout and have added mouse and keyboard functionality, we just need to add more accurate sound files and to be able to detect when the key isn't pressed via boolean and that would determine if there is a greenoutline on each key and when to stop playing a sound instead of it just keep going.

---
+++ PianoPilot2/src/main/MyPanel.java
@@ -0,0 +1,259 @@
+package src.main;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import src.bezel.Bezel;
+import src.piano.MyPiano;
+import src.piano.PlayKey;
+
+public class MyPanel extends JPanel implements Runnable{
+	
+	Thread appThread = new Thread();
+	
+	MyPiano myPiano = new MyPiano();
+	Bezel b = new Bezel();
+	PlayKey pk = new PlayKey();
+	MouseHandler mh = new MouseHandler(myPiano.bk, myPiano.wk);
+	KeyHandler kh = new KeyHandler();
+	
+	JLabel console = new JLabel();
+	ArrayList<String> outputs = new ArrayList<String>();
+	
+	
+	int FPS = 60;
+	
+	public static int screenWidth = 1920;
+	public static int screenHeight = 1080;
+	
+	public MyPanel() {
+		setPreferredSize(new Dimension(screenWidth,screenHeight));
+		setFocusable(true);
+
+		
+
+		add(b);
+		add(myPiano);
+		addMouseListener(mh);
+		addKeyListener(kh);
+
+		console.setBounds(100,100,200,30);
+		add(console);
+		console.setBackground(Color.black);
+        console.setForeground(Color.white);
+        console.setSize(200, 100);
+		
+
+	}
+
+	public void update(String note){
+		outputs.add(note);
+		console.setText(outputs.toString());
+	}
+/////////////////////////////////////////////////////////////////////////////////////////////////
+class KeyHandler implements KeyListener{
+				
+	@Override
+	public void keyPressed(KeyEvent e) {
+		int key = e.getKeyCode(); //registers the key typed
+		//white keys
+		if(key == KeyEvent.VK_A){
+			try {
+				pk.playSound("key09.wav");
+				update(myPiano.notes[0]);
+			} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+		}
+		if(key == KeyEvent.VK_S){
+			try {
+				pk.playSound("key10.wav");
+			} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e1) {
+				// TODO Auto-generated catch block
+				((Throwable) e1).printStackTrace();
+			}
+		
+			
+		}
+		if(key == KeyEvent.VK_D){
+			try {
+				pk.playSound("key11.wav");
+			} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+		}
+		if(key == KeyEvent.VK_F){
+			try {
+				pk.playSound("key12.wav");
+			} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+		}
+		if(key == KeyEvent.VK_G){
+			try {
+				pk.playSound("key13.wav");
+			} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+		}
+		if(key == KeyEvent.VK_J){
+			try {
+				pk.playSound("key14.wav");
+			} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+		}
+		if(key == KeyEvent.VK_K){
+			try {
+				pk.playSound("key15.wav");
+			} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+		}
+
+		//black keys
+		if(key == KeyEvent.VK_W){
+			try {
+				pk.playSound("key17.wav");
+			} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+		}
+		if(key == KeyEvent.VK_E){
+			try {
+				pk.playSound("key18.wav");
+			} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+		}
+		if(key == KeyEvent.VK_T){
+			try {
+				pk.playSound("key20.wav");
+			} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+		}
+		if(key == KeyEvent.VK_Y){
+			try {
+				pk.playSound("key21.wav");
+			} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+		}
+		if(key == KeyEvent.VK_U){
+			try {
+				pk.playSound("key22.wav");
+			} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+		}
+	
+	
+
+		
+	}
+
+
+
+	@Override
+	public void keyTyped(KeyEvent arg0){
+
+	}
+
+
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
+		
+	
+/////////////////////////////////////////////////////////////////////////////////////////////////
+	//App loop
+	public void startAppThread() {
+		appThread = new Thread(); // this is how you instantiate a thread
+		appThread.start(); // automatically calls the run method
+	}
+	
+	public void run() {// This will contain the main loop which will be the core of its performance
+		//Here we are using the sleep method to achieve 60 FPS
+        double drawInterval = 1000000000/FPS;// This is the total time between each frame 
+        double nextDrawTime = System.nanoTime() + drawInterval;
+        
+        	
+        	while(appThread != null) {
+        		//1,000,000,000 nanoseconds is equal to 1 second
+        		//long currentTime = System.nanoTime();//returns the current value of the running java virtual machines high-resolution time source, in nanoseconds
+        		
+        		
+        		update();// updates information 
+        		repaint();// Draws the screen with the updated information
+        		
+        		try {
+        			double remainingTime = nextDrawTime - System.nanoTime();
+        			
+        			if(remainingTime < 0) {
+        				remainingTime = 0;
+        			}
+        			Thread.sleep((long)remainingTime/1000000);
+        			nextDrawTime += drawInterval;
+        		}
+        		catch(InterruptedException e) {
+        			e.printStackTrace();
+        		}
+        	}
+	}
+	
+	
+	public void update() {
+		
+	}
+	
+	
+	public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+        
+        myPiano.draw(g2);
+        b.draw(g2);
+        
+        g2.dispose();//This disposes of this graphics context and release any system resources that it is using.
+    }
+
+
+}
---
+++ PianoPilot2/src/piano/MyWhiteKey.java
@@ -0,0 +1,19 @@
+package src.piano;
+
+import src.main.MyPanel;
+
+public class MyWhiteKey {
+	public int leftBoundX;
+    public int rightBoundX; 
+	static int width = (MyPanel.screenWidth)/14; // width of each white key
+	static int height = (MyPanel.screenHeight * 4)/5; // height of each key
+	
+	int octave;
+	
+	String note;
+	
+	
+	public MyWhiteKey() {
+		
+	}
+}
